c9aabc380c2590850e777b5878fdca82f9e5b697
Simão Neto <simaomenezesneto@gmail.com>
2024-01-26 23:43:51 +0000

    feat: Create and Setup Attachment camera boom on the ShooterCharacter

--- Source/UE4UltimateShooter/ShooterCharacter.cpp
+++ Source/UE4UltimateShooter/ShooterCharacter.cpp
@@ -2,12 +2,19 @@
 
 
 #include "ShooterCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
 AShooterCharacter::AShooterCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	// Create camera boom (pulls in towards the character if there is a collision)
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.f; // The camera follows at this distance behind the character
+	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
 
 }
 
