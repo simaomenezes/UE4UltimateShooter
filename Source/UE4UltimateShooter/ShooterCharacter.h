3dc3369db6525e329d4d66c98ab7621ae0f9dd6d
Simão Neto <simaomenezesneto@gmail.com>
2024-01-27 20:35:58 +0000

    feat: Add and implemented the Shooting Sound Effects

--- Source/UE4UltimateShooter/ShooterCharacter.h
+++ Source/UE4UltimateShooter/ShooterCharacter.h
@@ -64,6 +64,9 @@ private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	float BaseLookUpRate;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = "true"))
+	class USoundCue* FireSound;
+
 public:
 	/** Returns CameraBoom subobject */
 	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
