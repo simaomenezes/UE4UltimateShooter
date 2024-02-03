0b267eac97ad30dbb5c69d2c2148b196d2b34b69
Simão Neto <simaomenezesneto@gmail.com>
2024-02-03 22:19:24 +0000

    feat: Widget Trace When Close

--- Source/UE4UltimateShooter/ShooterCharacter.cpp
+++ Source/UE4UltimateShooter/ShooterCharacter.cpp
@@ -47,7 +47,9 @@ AShooterCharacter::AShooterCharacter() :
 	// Automatic fire variables
 	AutomaticFireRate(0.1f),
 	bShouldFire(true),
-	bFireButtonPressed(false)
+	bFireButtonPressed(false),
+	// Item trace variables
+	bShouldTraceForTtems(false)
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -533,6 +535,25 @@ bool AShooterCharacter::TraceUnderCrosshairs(FHitResult& OutHitResult, FVector& 
 	return false;
 }
 
+void AShooterCharacter::TraceForItems()
+{
+	if (bShouldTraceForTtems)
+	{
+		FHitResult ItemTraceResult;
+		FVector OutHitLocation;
+		TraceUnderCrosshairs(ItemTraceResult, OutHitLocation);
+		if (ItemTraceResult.bBlockingHit)
+		{
+			AItem* HitItem = Cast<AItem>(ItemTraceResult.Actor);
+			if (HitItem && HitItem->GetPickupWidget())
+			{
+				// Show Item's Pickup Widget
+				HitItem->GetPickupWidget()->SetVisibility(true);
+			}
+		}
+	}
+}
+
 // Called every frame
 void AShooterCharacter::Tick(float DeltaTime)
 {
@@ -546,19 +567,9 @@ void AShooterCharacter::Tick(float DeltaTime)
 
 	/** Calculate crosshair spread multiplier */
 	CalculateCrosshairSpread(DeltaTime);
-
-	FHitResult ItemTraceResult;
-	FVector OutHitLocation;
-	TraceUnderCrosshairs(ItemTraceResult, OutHitLocation);
-	if (ItemTraceResult.bBlockingHit)
-	{
-		AItem* HitItem = Cast<AItem>(ItemTraceResult.Actor);
-		if (HitItem && HitItem->GetPickupWidget())
-		{
-			// Show Item's Pickup Widget
-			HitItem->GetPickupWidget()->SetVisibility(true);
-		}
-	}
+	
+	/** Check OverlappedItemCount, then trace for items */
+	TraceForItems();
 
 }
 
@@ -591,5 +602,19 @@ void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 float AShooterCharacter::GetCrosshairSpreadMultiplier() const
 {
 	return CrosshairSpreadMultiplier;
+}
+
+void AShooterCharacter::IncrementOverlappedItemCount(int8 Amount)
+{
+	if (OverlappedItemCount + Amount <= 0)
+	{
+		OverlappedItemCount = 0;
+		bShouldTraceForTtems = false;
+	}
+	else
+	{
+		OverlappedItemCount += Amount;
+		bShouldTraceForTtems = true;
+	}
 }
 
--- Source/UE4UltimateShooter/ShooterCharacter.h
+++ Source/UE4UltimateShooter/ShooterCharacter.h
@@ -81,6 +81,9 @@ protected:
 	/** Line trace for items under the crosshair */
 	bool TraceUnderCrosshairs(FHitResult& OutHitResult, FVector& OutHitLocation);
 
+	/** Trace for items if OverlappedItemCount > 0 */
+	void TraceForItems();
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -212,6 +215,12 @@ private:
 	/** Sets a timer between gunshots */
 	FTimerHandle AutoFireTimer;
 
+	/** True if we should trace every frame for items */
+	bool bShouldTraceForTtems;
+
+	/** Number of overlapped AItems */
+	int8 OverlappedItemCount;
+
 
 public:
 	/** Returns CameraBoom subobject */
@@ -224,5 +233,10 @@ public:
 
 	UFUNCTION(BlueprintCallable)
 	float GetCrosshairSpreadMultiplier() const;
+
+	FORCEINLINE int8 GetOverlappedItemCount() const { return OverlappedItemCount; }
+
+	/** Adds / Subtracts to/from OverlappedItemCount and updates bShouldTraceForItems */
+	void IncrementOverlappedItemCount(int8 Amount);
 
 };
